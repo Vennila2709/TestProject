86e91d2d9ee26d688be2460ac0d56c0447879815
Vennila2709 <vennilapmn@gmail.com>
2022-04-12 09:24:17 +0530

    conflict

--- src/test/java/org/baseClass/BaseClass.java
+++ src/test/java/org/baseClass/BaseClass.java
@@ -60,6 +60,9 @@ public class BaseClass {
    public static void SBV(WebElement e, String value) {
 	   Select s=new Select(e);
 	   s.selectByValue(value);
+	   s.selectByValue(value);
+	   s.selectByValue(value);
+	   s.selectByValue(value);
    }
    
    public static void rightClick(WebElement e) {
